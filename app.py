393e9135f223e9cbe4a1761201e76be1367909b6
João Costa Seco <jrcs@fct.unl.pt>
2025-01-27 15:52:28 +0000

    Added a form to complete each item

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 list = ['Eggs', 'Milk', 'Bread', 'Butter']
 
 @app.route('/', methods=['GET'])
@@ -12,6 +11,12 @@ def index():  # put application's code here
 @app.route('/', methods=['POST'])
 def add():
     list.append(request.form['item'])
+    return index()
+
+@app.route('/complete', methods=['GET'])
+def complete():
+    value = request.args['index']
+    list.pop(int(value))
     return index()
 
 if __name__ == '__main__':
