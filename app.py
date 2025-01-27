f04f1eebcb2293bff8839e77fd0f7f057afe3f6c
João Costa Seco <jrcs@fct.unl.pt>
2025-01-27 12:31:05 +0000

    Added a form with a list in memory

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+list = ['Eggs', 'Milk', 'Bread', 'Butter']
+
+@app.route('/', methods=['GET'])
 def index():  # put application's code here
-    return render_template('index.html', list=['Eggs', 'Milk', 'Bread', 'Butter'])
+    return render_template('index.html', list=list)
+
+@app.route('/', methods=['POST'])
+def add():
+    list.append(request.form['item'])
+    return index()
 
 if __name__ == '__main__':
     app.run()
